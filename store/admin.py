0794764a4789c7720a22b71ce728631081699a88
Tanner Schmutte <tannerschmutte@gmail.com>
2021-08-10 21:44:28 -0700

    eager load with list_select_related

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -20,6 +24,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
 
 
 admin.site.register(models.Collection)
