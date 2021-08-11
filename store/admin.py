d31c8a8bfccba3441c5d67e1af9421cace8333f5
Tanner Schmutte <tannerschmutte@gmail.com>
2021-08-11 15:36:26 -0700

    add TagInLine to add product page

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models.aggregates import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
-from . import models
+from . import models 
+from tags.models import TaggedItem
 
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -41,6 +43,11 @@ class CollectionAdmin(admin.ModelAdmin):
         )
 
 
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -49,6 +56,7 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
+    inlines = [TagInline]
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
