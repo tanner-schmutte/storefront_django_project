ee969b64115cf29286c8d6acad757646c5c2fa36
Tanner Schmutte <tannerschmutte@gmail.com>
2021-08-11 15:49:04 -0700

    add store_custom app to decouple store and tags apps

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin, messages
-from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models.aggregates import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from . import models 
-from tags.models import TaggedItem
 
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -43,12 +41,6 @@ class CollectionAdmin(admin.ModelAdmin):
         )
 
 
-class TagInline(GenericTabularInline):
-    autocomplete_fields = ['tag']
-    model = TaggedItem
-
-
-
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     autocomplete_fields = ['collection']
@@ -56,7 +48,6 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
-    inlines = [TagInline]
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
